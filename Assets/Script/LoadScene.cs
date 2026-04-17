4d80d4c18e4d4a49b8ce670e24a0e803d6332c78
Raiki <rw0124@icloud.com>
2026-04-17 19:34:00 +0900

    コードの勉強

---
+++ Assets/Script/LoadScene.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    [SerializeField] private 
+}
---
+++ Assets/Script/Test/Controller.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Controller : MonoBehaviour
+{
+    private InputAction Q;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Q= GetComponent<InputAction>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(InputAction.)
+    }
+}
