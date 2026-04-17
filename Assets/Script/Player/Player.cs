004fd0fd612dff08bb374eb417752d20a797a9ad
OkuyamaTaiki <ktc25a31a0014@edu.kyoto-tech.ac.jp>
2026-04-17 19:35:06 +0900

    コードのお勉強

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -29,23 +29,10 @@ public class PlayerMove : MonoBehaviour
             Vector3 moveForward = cameraForeard * value.y + Camera.main.transform.right * value.x;
             rb.linearVelocity = new float3(moveForward) * (input.actions["Sprint"].inProgress ? 4 : 2);
 
-            float current = 0;
-            //回転量の計算
-            var rotAngle = Mathf.SmoothDampAngle(
-                0,
-                Vector3.Angle(transform.forward, moveForward),//ベクトル差の計算
-                ref current,
-                0.05f,          //進行方向を向くのにかかる時間
-                Mathf.Infinity  //最高回転角度
-                );
-            //回転の計算
-            var nextRot = Quaternion.RotateTowards(
-                rb.rotation,    //現在角度
-                Quaternion.LookRotation(moveForward, Vector3.up),//進行方向へのクォータニオン取得
-                rotAngle
-                );
-            //反映
-            rb.rotation = nextRot;
+            rb.rotation = Quaternion.RotateTowards(
+                rb.rotation,
+                Quaternion.LookRotation(moveForward),
+                360 * Time.deltaTime);
         }
 
         if (input.actions["Q"].IsPressed())
